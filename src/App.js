fa182765a560bffc734af7c1ea5acca5feba1734
Ruge Li <rugeli0605@gmail.com>
2022-06-21 14:52:52 -0700

    toggle complete

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {useState} from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
 
@@ -16,13 +17,31 @@ const TASKS = [
 ];
 
 const App = () => {
+  const [complete, setComplete] = useState(TASKS);
+
+  const updateComplete = (id) => {
+    console.log('inside updateComplete', id);
+    const newComplete = complete.map((task) => {
+      const newTask = {...task};  
+      if (newTask.id === id) {
+        newTask.isComplete = !newTask.isComplete;
+      }
+      return newTask;
+  });
+    console.log(newComplete);
+    setComplete(newComplete);
+  };
+
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>
+          <TaskList tasks={complete} setCompleteCallback={updateComplete}/>
+        </div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -3,15 +3,24 @@ import PropTypes from 'prop-types';
 
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
+const Task = ({ id, title, isComplete, setCompleteCallback}) => {
+  // const [complete, setComplete] = useState(isComplete);
+  const complete = isComplete;
   const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+
+
+  const toggleComplete = () => {
+    setCompleteCallback(id);
+    // setComplete(!complete);
+
+  };
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        // onClick={() => setComplete(!complete)}
+          onClick={() => toggleComplete()}
       >
         {title}
       </button>
@@ -24,6 +33,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  setCompleteCallback: PropTypes.func.isRequired
 };
 
 export default Task;
